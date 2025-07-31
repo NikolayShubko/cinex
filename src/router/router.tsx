3c3a6a6c458e38bb6980dad6fa482e2a38c8a040
Nikolay Shubko <nshubcko@gmail.com>
2025-07-31 19:47:19 +0300

    Navigation Refactor

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,56 +7,108 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home endpoint="/api/v1/films/latest " />,
+    //     children: [
+    //       {
+    //         path: "new",
+    //         element: <Home endpoint="api/v1/films/latest " />,
+    //       },
+    //       {
+    //         path: "most-popular",
+    //         element: <Home endpoint="api/v1/films/" />,
+    //       },
+    //       {
+    //         path: "keep-watching",
+    //         element: <Home endpoint="api/v1/films/" />,
+    //       },
+    //       {
+    //         path: "recommended-for-you",
+    //         element: <Home endpoint="api/v1/films/" />,
+    //       },
+    //       {
+    //         path: "trending-now",
+    //         element: <Home endpoint="api/v1/films/" />,
+    //       },
+    //     ],
+    //   },
+
+    //   {
+    //     path: "/movies",
+    //     element: <Movies endpoint="api/v1/films/onlyFilms" />,
+    //     children: [
+    //       {
+    //         path: "new",
+    //         element: <Movies endpoint="api/v1/films/onlyFilms" />,
+    //       },
+    //       {
+    //         path: "most-popular",
+    //         element: <Movies endpoint="api/v1/films/onlyFilms" />,
+    //       },
+    //       {
+    //         path: "keep-watching",
+    //         element: <Movies endpoint="api/v1/films/onlyFilms" />,
+    //       },
+    //       {
+    //         path: "recommended-for-you",
+    //         element: <Movies endpoint="api/v1/films/onlyFilms" />,
+    //       },
+    //     ],
+    //   },
+    // ],
+
     children: [
       {
         path: "/",
-        element: <Home endpoint="/api/v1/films/latest " />,
-        children: [
-          {
-            path: "new",
-            element: <Home endpoint="api/v1/films/latest " />,
-          },
-          {
-            path: "most-popular",
-            element: <Home endpoint="api/v1/films/" />,
-          },
-          {
-            path: "keep-watching",
-            element: <Home endpoint="api/v1/films/" />,
-          },
-          {
-            path: "recommended-for-you",
-            element: <Home endpoint="api/v1/films/" />,
-          },
-          {
-            path: "trending-now",
-            element: <Home endpoint="api/v1/films/" />,
-          },
-        ],
+        element: <Home endpoint="/api/v1/films/" />,
       },
-
+      {
+        path: "/new",
+        element: <Home endpoint="/api/v1/films/latest" />,
+      },
+      {
+        path: "/most-popular",
+        element: <Home endpoint="/api/v1/films/highestRated" />,
+      },
+      {
+        path: "/keep-watching",
+        element: <Home endpoint="/api/v1/films/" />,
+      },
+      {
+        path: "/recommended-for-you",
+        element: <Home endpoint="/api/v1/films/" />,
+      },
+      {
+        path: "/trending-now",
+        element: <Home endpoint="/api/v1/films/" />,
+      },
       {
         path: "/movies",
-        element: <Movies endpoint="api/v1/films/onlyFilms" />,
-        children: [
-          {
-            path: "new",
-            element: <Movies endpoint="api/v1/films/onlyFilms" />,
-          },
-          {
-            path: "most-popular",
-            element: <Movies endpoint="api/v1/films/onlyFilms" />,
-          },
-          {
-            path: "keep-watching",
-            element: <Movies endpoint="api/v1/films/onlyFilms" />,
-          },
-          {
-            path: "recommended-for-you",
-            element: <Movies endpoint="api/v1/films/onlyFilms" />,
-          },
-        ],
+        element: <Movies endpoint="/api/v1/films/onlyFilms" />,
+      },
+      {
+        path: "/movies/new",
+        element: <Movies endpoint="/api/v1/films/onlyFilms/latest" />,
+      },
+      {
+        path: "/movies/most-popular",
+        element: <Movies endpoint="/api/v1/films/onlyFilms/highestRated" />,
+      },
+      {
+        path: "/movies/keep-watching",
+        element: <Movies endpoint="/api/v1/films" />,
+      },
+      {
+        path: "/movies/recommended-for-you",
+        element: <Movies endpoint="/api/v1/films/onlyFilms" />,
+      },
+      {
+        path: "/movies/trending-now",
+        element: <Movies endpoint="/api/v1/films" />,
       },
     ],
   },
+  { path: "*", element: <div>Not Found Sorry</div> },
 ]);
